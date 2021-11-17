0fb15fc8f726f8f16b142998303e7f2c1c50b3b2
ttsang96 <taitatsang@gmail.com>
2021-11-17 17:58:58 +0700

    add redis cluster config

--- src/main/java/com/paydi/config/other/RedisConfig.java
+++ src/main/java/com/paydi/config/other/RedisConfig.java
@@ -1,26 +1,45 @@
 package com.paydi.config.other;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
+import io.lettuce.core.ReadFrom;
+
 @Configuration
 public class RedisConfig {
-    @Value("${spring.redis.host}")
-    private String redisHost;
 
-    @Value("${spring.redis.port}")
-    private int redisPort;
+    @Autowired
+    private ClusterConfigurationProperties clusterProperties;
 
     @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        // Tạo Standalone Connection tới Redis
-        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(redisHost, redisPort));
+    LettuceConnectionFactory redisConnectionFactory(RedisClusterConfiguration redisConfiguration) {
+
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .readFrom(ReadFrom.REPLICA_PREFERRED).build();
+
+        return new LettuceConnectionFactory(redisConfiguration, clientConfig);
+    }
+
+    @Bean
+    RedisClusterConfiguration redisConfiguration() {
+        RedisClusterConfiguration redisClusterConfiguration = new RedisClusterConfiguration(
+                clusterProperties.getNodes());
+        redisClusterConfiguration.setMaxRedirects(clusterProperties.getMaxRedirects());
+
+        return redisClusterConfiguration;
     }
 
     @Bean
